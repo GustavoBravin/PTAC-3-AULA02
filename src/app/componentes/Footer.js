00cc1b1190cee2e6e2096646de2e0be1393e66b1
GustavoBravin <gugarbravin@gmail.com>
2024-02-28 14:31:29 -0400

    28/02/2024

--- src/app/componentes/Footer.js
+++ src/app/componentes/Footer.js
@@ -1,8 +1,16 @@
 import Link from "next/link"
+import styles from "./menu.module.css";
+import Image from "next/image";
 
 export default function Footer(){
 return(
-    <footer>
+    <footer className={styles.footer}>
+
+        <Image  
+         width={100}
+         height={100}
+         src={"https://www.ifms.edu.br/marcaifms.png"}
+         />
          <h1>Footer</h1>
     </footer>
 )
