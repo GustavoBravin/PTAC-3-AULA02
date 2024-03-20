985226741f2adb49d702fdb731c241440fb28e65
GustavoBravin <gugarbravin@gmail.com>
2024-03-20 14:16:47 -0400

    salve amem
    dia 20/03

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
+const url = "https://back-end-ifms-omega.vercel.app/campi";
 
 export default async function Home() {
 
+  const resposta = await fetch(url,{
+    cache:"no-cache"
+  });
+  const campi = await resposta.json();
+ 
   return (
     <main>
       <h1>Home</h1>
+      {campi.map((campus) =>
+        <div>
+             <p>{campus.nome_campus}</p>
+        </div>
+      )}
     </main>
   )
 }
